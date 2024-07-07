239afee3066aa93222d455345d1d4df6c6319090
Yi Lu <yi.lu@neuralink.com>
2024-07-06 21:53:27 -0700

    Capture error code

--- simpleble/src/backends/macos/AdapterBaseMacOS.mm
+++ simpleble/src/backends/macos/AdapterBaseMacOS.mm
@@ -63,8 +63,8 @@
     policy = SCHED_RR;
     param.sched_priority = 50; // Adjust as needed, between 1 and 63 for SCHED_RR
     
-    if (pthread_setschedparam(thread, policy, &param) != 0) {
-        int err = errno;
+    int err = pthread_setschedparam(thread, policy, &param);
+    if (err != 0) {
         NSLog(@"Failed to set thread scheduling parametersm errno: %d", err);
     } else {
         NSLog(@"Set thread policy: SCHED_RR, priority: %d", param.sched_priority);
